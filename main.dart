3506598fb1cde6df4571df70ab9e29bc435724be
Angel <63896621+angellhj@users.noreply.github.com>
2022-11-12 17:19:51 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -44,7 +44,7 @@ class NavBar extends StatefulWidget {
 }
 
 class _NavBarState extends State<NavBar> {
-  var _currentTab = PageItem.Home;
+  var _currentPage = PageItem.Home;
   final _navigatorKeys = {
     PageItem.Home: GlobalKey<NavigatorState>(),
     PageItem.Order: GlobalKey<NavigatorState>(),
@@ -52,68 +52,64 @@ class _NavBarState extends State<NavBar> {
     PageItem.Account: GlobalKey<NavigatorState>(),
   };
 
-  void _selectTab(PageItem pageItem) {
-    if (pageItem == _currentTab) {
+  void _selectPage(PageItem pageItem) {
+    if (pageItem == _currentPage) {
       // pop to first route
       _navigatorKeys[PageItem]!.currentState!.popUntil((route) => route.isFirst);
     } else {
-      setState(() => _currentTab = pageItem);
+      setState(() => _currentPage = pageItem);
     }
   }
+
+  static const List<Widget> _pages = <Widget>[
+    HomePage(title: 'Home'),
+    OrderPage(title: 'Order'),
+    ChatPage(title: 'Chat'),
+    AccountPage(title: 'Account'),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async {
-        final isFirstRouteInCurrentTab =
-        !await _navigatorKeys[_currentTab]!.currentState!.maybePop();
-        if (isFirstRouteInCurrentTab) {
-          // if not on the 'main' tab
-          if (_currentTab != PageItem.Home) {
-            // select 'main' tab
-            _selectTab(PageItem.Home);
+        final isFirstRouteInCurrentPage =
+        !await _navigatorKeys[_currentPage]!.currentState!.maybePop();
+        if (isFirstRouteInCurrentPage) {
+          // if not on the 'main' Page
+          if (_currentPage != PageItem.Home) {
+            // select 'main' Page
+            _selectPage(PageItem.Home);
             // back button handled by app
             return false;
           }
         }
         // let system handle back button if we're on the first route
-        return isFirstRouteInCurrentTab;
+        return isFirstRouteInCurrentPage;
       },
       child: Scaffold(
-        body: Stack(children: <Widget>[
-          _buildOffstageNavigator(PageItem.Home),
-          _buildOffstageNavigator(PageItem.Order),
-          _buildOffstageNavigator(PageItem.Chat),
-          _buildOffstageNavigator(PageItem.Account),
-        ]),
+        body: Center(
+          child: _pages.elementAt(_currentPage.index),
+        ),
         bottomNavigationBar: BottomNavigation(
-          currentTab: _currentTab,
-          onSelectTab: _selectTab,
+          currentPage: _currentPage,
+          onSelectPage: _selectPage,
         ),
       ),
     );
   }
 
-  Widget _buildOffstageNavigator(PageItem pageItem) {
-    return Offstage(
-      offstage: _currentTab != pageItem,
-      child: TabNavigator(
-        navigatorKey: _navigatorKeys[pageItem],
-        pageItem: pageItem,
-      ),
-    );
-  }
-}
-
 class BottomNavigation extends StatelessWidget {
-  const BottomNavigation({super.key, required this.currentTab, required this.onSelectTab});
-  final PageItem currentTab;
-  final ValueChanged<PageItem> onSelectTab;
+  const BottomNavigation({super.key, required this.currentPage, required this.onSelectPage});
+  final PageItem currentPage;
+  final ValueChanged<PageItem> onSelectPage;
   final int _selectedIndex = 0;
   
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
+      iconSize: 30,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
       type: BottomNavigationBarType.fixed,
       items: [
         BottomNavigationBarItem(
@@ -145,72 +141,15 @@ class BottomNavigation extends StatelessWidget {
           label: ('Account'),
         ),
       ],
-      onTap: (index) => onSelectTab(
+      onTap: (index) => onSelectPage(
         PageItem.values[index],
       ),
-      currentIndex: currentTab.index,
+      currentIndex: currentPage.index,
       selectedItemColor: Colors.orange.shade400,
     );
   }
 
   Color _onCurrentPage(PageItem item) {
-    return currentTab == item ? Colors.orange : Colors.grey;
-  }
-}
-
-class TabNavigatorRoutes {
-  static const String root = '/';
-}
-
-class TabNavigator extends StatelessWidget {
-  const TabNavigator({super.key, required this.navigatorKey, required this.pageItem});
-  final GlobalKey<NavigatorState>? navigatorKey;
-  final PageItem pageItem;
-
-  void _push(BuildContext context) {
-    var routeBuilders = _routeBuilders(context);
-
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) =>
-            routeBuilders[TabNavigatorRoutes.root]!(context),
-      ),
-    );
-  }
-
-  Map<String, WidgetBuilder> _routeBuilders(BuildContext context) {
-    return {
-      TabNavigatorRoutes.root: (context) => HomePage(
-        title: pageItem.name,
-        onPush: (materialIndex) => _push(context),
-      ),
-      TabNavigatorRoutes.root: (context) => OrderPage(
-        title: pageItem.name,
-        onPush: (materialIndex) => _push(context),
-      ),
-      TabNavigatorRoutes.root: (context) => ChatPage(
-        title: pageItem.name,
-        onPush: (materialIndex) => _push(context),
-      ),
-      TabNavigatorRoutes.root: (context) => AccountPage(
-        title: pageItem.name,
-        onPush: (materialIndex) => _push(context),
-      ),
-    };
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final routeBuilders = _routeBuilders(context);
-    return Navigator(
-      key: navigatorKey,
-      initialRoute: TabNavigatorRoutes.root,
-      onGenerateRoute: (routeSettings) {
-        return MaterialPageRoute(
-          builder: (context) => routeBuilders[routeSettings.name!]!(context),
-        );
-      },
-    );
+    return currentPage == item ? Colors.orange : Colors.grey;
   }
 }
